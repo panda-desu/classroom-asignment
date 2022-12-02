833f7142d0d32ab12ebede22665a7f3cd75f5976
Sedka08 <115856472+panda-desu@users.noreply.github.com>
2022-12-02 14:51:11 +0800

    Update 1.js
    hello

--- js number/2too/1.js
+++ js number/2too/1.js
@@ -1,5 +1,5 @@
-let a = prompt('a toog bichne uu')
-let b = prompt('b toog bichne uu')
+let a = Number(prompt('a toog bichne uu'))
+let b = Number(prompt('b toog bichne uu'))
 let min; let max;
 console.log('hamgiin ih too', max = Math.max(a, b,))
 console.log('hamgiin baga too', min = Math.min(a, b))
