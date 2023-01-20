a170e3e49d1cafdfcee4bba331ff1d64ca4f1c27
Sed-Erdene <zr.freeze05@gmail.com>
2023-01-20 17:10:54 +0800

    hi

---
+++ react-admin-panel/blog/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { Card } from "react-bootstrap";
+
+export const Home = () => {
+  const articles = [];
+  return (
+    <main>
+      <div className="container">
+        <div className="row">
+          {articles.map((article) => (
+            <div className="col-md-3 col-sm-6 col-12">
+              <Card title={article.title} image={article.image} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
