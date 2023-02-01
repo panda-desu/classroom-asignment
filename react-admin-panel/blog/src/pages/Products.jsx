f256211d1593c9d2f19ff30b64e6806c4c9774d6
Sed-Erdene <zr.freeze05@gmail.com>
2023-02-01 17:34:56 +0800

    1

--- react-admin-panel/blog/src/pages/Products.jsx
+++ react-admin-panel/blog/src/pages/Products.jsx
@@ -1,17 +1,36 @@
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import dayjs from "dayjs";
+
 import relateTime from "dayjs/plugin/relativeTime";
 import { currencyFormatter } from "../utils/currencyFormatter";
+import { Link, useLocation } from "react-router-dom";
 dayjs.extend(relateTime);
 export default function Product() {
   const [page, setPage] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(12);
+
+  const location = useLocation();
+
   useEffect(() => {
-    axios.get("http://localhost:8000/products?pageSize=12").then((res) => {
-      setPage(res.data);
-    });
-  }, []);
+    axios
+      .get(
+        `http://localhost:8000/products?pageSize=${pageSize}&page=${currentPage}`
+      )
+      .then((res) => {
+        setPage(res.data);
+      });
+  }, [currentPage, pageSize]);
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    if (searchParams.has("page")) {
+      setCurrentPage(Number(searchParams.get("page")));
+    }
+    if (searchParams.has("page")) {
+      setPageSize(Number(searchParams.get("pageSize")));
+    }
+  }, [location]);
   useEffect(() => {
     console.log(page);
   }, [page]);
@@ -25,21 +44,116 @@ export default function Product() {
   }
   const getPaginations = () => {
     let result = [];
-    for (let i = 1; i <= page.totalPages; i++) {
+    // first page adding
+    result.push(
+      <li className={`page-item ${1 === page.page && "active"}`}>
+        <a className="page-link" href="#">
+          1
+        </a>
+      </li>
+    );
+    // front trible dots
+    if (page.page - 3 > 0) {
       result.push(
-        <li className={`page-item ${i === page.page && "active"}`}>
-          <a className="page-link" href="#">
-            {i}
+        <li className={`page-item`}>
+          <span className="page-link">...</span>
+        </li>
+      );
+    }
+    if (page.page !== 1 && page.page !== page.totalPages) {
+      result.push(
+        <>
+          {currentPage === 2 ? (
+            <></>
+          ) : (
+            <li className={`page-item active`}>
+              <a href="#" className="page-link">
+                {page.page - 1}
+              </a>
+            </li>
+          )}
+          <li className={`page-item active`}>
+            <a href="#" className="page-link">
+              {page.page}
+            </a>
+          </li>
+          {""}
+          {currentPage === page.totalPages - 1 ? (
+            <></>
+          ) : (
+            <li className={`page-item`}>
+              <a className="page-link" href="#">
+                {page.page + 1}
+              </a>{" "}
+            </li>
+          )}
+        </>
+      );
+    }
+    if (page.page === 1) {
+      result.push(
+        <li className={`page-item`}>
+          <span className="page-link">2</span>
+        </li>
+      );
+    }
+
+    if (page.page === pageSize) {
+      result.push(
+        <li className={`page-item`}>
+          <span className="page-lnk">{page.totalPages - 1}</span>
+        </li>
+      );
+    }
+
+    if (page.page === 1 && page.page === 2) {
+      result.push(
+        <li className={`page-item active`}>
+          <a href="#" className="page-link">
+            2
           </a>
         </li>
       );
     }
+
+    if (page.totalPages - 3 >= page.page) {
+      // back trible dots
+      result.push(
+        <li className={`page-item`}>
+          <span className="page-link">...</span>
+        </li>
+      );
+    }
+    // last page adding
+    result.push(
+      <li className={`page-item ${page.totalPages === page.page && "active"}`}>
+        <a className="page-link" href="#">
+          {page.totalPages}
+        </a>
+      </li>
+    );
     return result;
   };
 
   return (
     <main>
       <div className="container">
+        <div className="d-flex justify-content-end mb-4">
+          <label>
+            Хуудаслалт &nbsp;
+            <select
+              className="form-control d-inline-block w-auto"
+              onChange={(e) => setPageSize(e.target.value)}
+              value={pageSize}
+            >
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>
+          </label>
+        </div>
         <div className="row gy-4">
           {page.items.map((products) => {
             return (
@@ -66,21 +180,26 @@ export default function Product() {
       <div>
         <nav aria-label="..." className="my-4">
           <ul className="pagination pagination-lg justify-content-center">
-            <li className={`page-item ${page.page === 1 && "disabled"}`}>
-              <a href="#" className="page-link">
+            <li className={`page-item ${currentPage === 1 && "disabled"}`}>
+              <Link
+                to={`/products?pageSize=${pageSize}&page=${currentPage - 1}`}
+                className="page-link"
+              >
                 Previous
-              </a>
+              </Link>
             </li>
             {getPaginations()}
-
             <li
               className={`page-item ${
-                page.page === page.totalPages && "disabled"
+                currentPage === page.totalPages && "disabled"
               }`}
             >
-              <a className="page-link" href="#">
+              <Link
+                to={`/products?pageSize=${pageSize}&page=${currentPage + 1}`}
+                className="page-link"
+              >
                 Next
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
